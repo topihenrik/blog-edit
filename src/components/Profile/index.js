30cc8483935a2a856f08485e9f290de2916fc78e
Topi Ilmoni <topi.ilmoni@gmail.com>
2022-10-05 23:02:50 +0300

    Image src was changed to support CDN url

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -88,7 +88,7 @@ export default function Profile(props) {
                                 </div>
                                 <div>
                                     
-                                        <img id="author-avatar-profile" src={`${process.env.REACT_APP_API_URL}/`+result?.user?.avatar?.path}/>
+                                        <img id="author-avatar-profile" src={result?.user?.avatar?.url}/>
                                     </div>
                             </div>
                         </div>
